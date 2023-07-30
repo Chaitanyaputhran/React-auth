a43e5e04c14f192b180211d649d08fbb95d5a1c5
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-07-30 16:15:25 +0530

    updated

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import styles from "./Loginmodule.module.css"; // Import CSS module classes
+import styles from "./Loginmodule.module.css"; 
+import InputControl from "../inputControl/inputControl"
 
 function login() {
   return (
-    <div className={styles.container}> {/* Use curly braces for className and include styles */}
-      <div className={styles.innerBox}> {/* Use curly braces for className and include styles */}
-        <h1 className={styles.heading}>Login</h1> {/* Use curly braces for className and include styles */}
+    <div className={styles.container}>
+      <div className={styles.innerBox}>
+        <h1 className={styles.heading}>Login</h1> 
+        <InputControl label="Email" placeholder="Enter email address"/>
+        <InputControl label="Password" placeholder="Enter Password"/>
+        <div className={styles.footer}>
+          <button>Login</button>
+          <p>Already have an account?<span><Link to="/Signup">
+          </Link>Sign up</span></p>
+        </div>
       </div>
     </div>
   );
