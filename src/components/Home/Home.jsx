10abb55389983424b3e483f9d2e9f5f36ed07353
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-07-30 18:06:38 +0530

    updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.module.css';
 
 function Home(props) {
   return (
-    <div>
+    <div clasName="Container">
       <div >
         <h1>
           <Link to="/login">Login</Link>
